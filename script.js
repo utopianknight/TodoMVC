9573537b2ea7171ebacbb8bc82fe9476a16b0f3a
Sawan <thesithlord2007@gmail.com>
2017-05-05 11:27:36 +0400

    display complete status with output

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ var todoList = {
 		} else {
 			console.log('My todos:');
 			for (var i = 0; i < this.todos.length; i++) {
-				console.log(this.todos[i].todoText);
+				if (this.todos[i].completed === true) {
+					console.log('(x)', this.todos[i].todoText);
+				} else {
+					console.log('( )', this.todos[i].todoText);
+				}
 			}
 		}
 	},
