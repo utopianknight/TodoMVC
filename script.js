d94243702ec296cd30aa49dc4816fdb05be65b7a
Sawan <thesithlord2007@gmail.com>
2017-05-05 11:05:16 +0400

    corrected display

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var todoList = {
 	todos: [],
 	displayTodos: function () {
-		console.log('My todos:', this.todos);
+		for (var i = 0; i < this.todos.length; i++) {
+			console.log(this.todos[i].todoText);
+		}
 	},
 	addTodo: function(todoText) {
 		this.todos.push({
