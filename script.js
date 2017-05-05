2cf0de60a4a2d111c30101f65f799a8f317dbb06
Sawan <thesithlord2007@gmail.com>
2017-05-05 11:48:01 +0400

    toggle all

--- script.js
+++ script.js
@@ -33,5 +33,27 @@ var todoList = {
 		var todo = this.todos[position];
 		todo.completed = !todo.completed;
 		this.displayTodos();
+	},
+	toggleAll: function() {
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+
+		if (completedTodos === totalTodos) {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		} else {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
+
+		this.displayTodos();
 	}
 }
