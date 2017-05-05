6149179e1ab405beba7513cd374df96a9c17222e
Sawan <thesithlord2007@gmail.com>
2017-05-05 10:41:29 +0400

    changed add string to object with completed status + toggle

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 var todoList = {
-	todos: ['item 1', 'item 2', 'item 3'],
+	todos: [],
 	displayTodos: function () {
 		console.log('My todos:', this.todos);
 	},
-	addTodo: function(todo) {
-		this.todos.push(todo);
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
 		this.displayTodos();
 	},
-	changeTodo: function(position, newValue) {
-		this.todos[position] = newValue;
+	changeTodo: function(position, todoText) {
+		this.todos[position].todoText = todoText;
 		this.displayTodos();
 	},
 	removeTodo: function(position){
-		this.todos.splice(position, 1);
+		this.todos.splice(position-1, 1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
 		this.displayTodos();
 	}
 }
-
-todoList.removeTodo(1);
-
-
