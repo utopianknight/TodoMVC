e0d31c6d60ed7ddd834063030ab4fde9b6de7e70
Sawan <thesithlord2007@gmail.com>
2017-05-05 11:15:20 +0400

    Output 'todos list empty' in case todos list is empty

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var todoList = {
 	todos: [],
 	displayTodos: function () {
-		for (var i = 0; i < this.todos.length; i++) {
-			console.log(this.todos[i].todoText);
+		if (this.todos.length === 0) {
+			console.log('ToDos list is empty');
+		} else {
+			console.log('My todos:');
+			for (var i = 0; i < this.todos.length; i++) {
+				console.log(this.todos[i].todoText);
+			}
 		}
 	},
 	addTodo: function(todoText) {
