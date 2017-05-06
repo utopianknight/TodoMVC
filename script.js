728b68b7e1888ce9052a227f7930d6ae28d74712
Sawan <thesithlord2007@gmail.com>
2017-05-06 23:11:48 +0400

    add a toggle button and field

--- script.js
+++ script.js
@@ -93,5 +93,10 @@ var handlers = {
 		var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
 		todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
 		deleteTodoPositionInput.value = '';
+	},
+	toggleCompleted: function() {
+		var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+		todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+		toggleCompletedPositionInput.value = '';
 	}
 };
