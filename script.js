9f3e63efbfeaee088fdfb4496829bc300c8445f1
Sawan <thesithlord2007@gmail.com>
2017-05-06 21:34:26 +0400

    change button to onclick run functions instead of DOM manipulation

--- script.js
+++ script.js
@@ -58,14 +58,23 @@ var todoList = {
 	}
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
+// var displayTodosButton = document.getElementById('displayTodosButton');
 
-var toggleAllButton = document.getElementById('toggleAllButton');
+// var toggleAllButton = document.getElementById('toggleAllButton');
 
-displayTodosButton.addEventListener('click', function() {
-	todoList.displayTodos();
-});
+// displayTodosButton.addEventListener('click', function() {
+// 	todoList.displayTodos();
+// });
 
-toggleAllButton.addEventListener('click', function() {
-	todoList.toggleAll();
-});
+// toggleAllButton.addEventListener('click', function() {
+// 	todoList.toggleAll();
+// });
+
+var handlers = {
+	displayTodos: function() {
+		todoList.displayTodos();
+	},
+	toggleAll: function() {
+		todoList.toggleAll();
+	}
+};
