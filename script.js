ef60c9d8ea61ef6ffac152bb33d32aebd0234df9
Sawan <thesithlord2007@gmail.com>
2017-05-07 11:52:24 +0400

    display todos in a list in html

--- script.js
+++ script.js
@@ -100,3 +100,15 @@ var handlers = {
 		todoList.toggleAll();
 	}
 };
+
+var view = {
+	displayTodos: function() {
+		var todosUl = document.querySelector('ul');
+		todosUl.innerHTML = '';
+		for (var i = 0; i < todoList.todos.length; i++) {
+			var todoLi = document.createElement('li');
+			todoLi.textContent = todoList.todos[i].todoText;
+			todosUl.appendChild(todoLi);
+		}
+	}
+};
