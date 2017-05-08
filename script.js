a96fe304a384a5440d3b6188107a6dc6fe058081
Sawan <thesithlord2007@gmail.com>
2017-05-08 12:33:18 +0400

    delete button logs id of li

--- script.js
+++ script.js
@@ -99,3 +99,9 @@ var view = {
 		return deleteButton;
 	}
 };
+
+var todosUl = document.querySelector('ul');
+
+todosUl.addEventListener('click', function(event) {
+	console.log(event.target.parentNode.id);
+});
